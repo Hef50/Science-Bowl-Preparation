2bf375cb988ab34224a564e930f7f1af654e617b
Hef50 <70100891+Hef50@users.noreply.github.com>
2020-08-23 07:26:55 -0700

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,98 @@
+LifeSQ1 = {
+    "What organelle functions to isolate a human cell’s chromosomes from the cytoplasm?":
+    "NUCLEUS",
+    "What is the primary oxygen-carrying protein found in red blood cells?":
+    "HEMOGLOBIN",
+    "How many permanent teeth does a typical adult human have?":
+    "32",
+    "How many canine teeth does the typical human adult have?":
+    "4",
+    "Most of the heart is composed of which of the following types of tissues: epithelial, connective, nervous, or muscle":
+    "MUSCLE",
+    "In the taxonomical name Homo sapiens, what taxonomical rank does sapiens represent?":
+    "SPECIES",
+    "What is generally considered the basic unit of life?":
+    "CELL",
+    "In what organelle of a plant cell does photosynthesis occur?":
+    "CHLOROPLAST",
+    "What is the scientific name for the white of a cooked egg?":
+    "ALBUMEN",
+    "What term is generally used for biological catalysts?":
+    "ENZYMES",
+    "What is the scientific name of the phylum characterized by thevpresence of jointed appendages?": "ARTHROPODA",
+    "What is the name of the blood vessels in vertebrate bodies where most of the exchange of oxygen and carbon dioxide occurs? (Plural)": "CAPILLARIES",
+    "What specific part of the human brain is most responsible for regulating body temperature?": "HYPOTHALAMUS",
+    "Name all of the following 4 organisms that are echinoderms: squid; snails; starfish; lobsters": "STARFISH",
+    "What parts of a flower make up the corolla?": "PETALS",
+    "How many chambers does the heart of a shark have?": "2",
+    "What scientist is sometimes considered the father of genetics because of his extensive studies on the patterns of inheritance? (Full Name) ": "GREGOR MENDEL",
+    "How many phalanges are in a human thumb?": "2",
+    " In what organelle do parenchymal (read as: PAR-en-KYM-al) cells store most of a plant’s cellular fluid?": "VACUOLE",
+    "What glands of the human body are most responsible for producing what is sometimes called the stress hormone cortisol? (Singular)": "ADRENAL"
+}
+EarthSQ1 = {
+    "What is the general name for the rising and falling of sea levels in response to the forces exerted by the Moon and Sun?": "TIDES",
+    "Approximately what fraction of Earth’s surface is covered by oceans: one-half, two-thirds, seven-tenths, or four-fifths ": "SEVEN-TENTHS",
+    "The hydrologic cycle describes the circulation of Earth’s: tides, water, sediments, or rocks": "WATER",
+    "What is the name of the rock that makes up most of the ocean floor and volcanic ocean islands?": "BASALT",
+    "Which of the Gas giant planets is closest to the Sun?": "JUPITER ",
+    "What are the 2 main factors causing the metamorphism of rocks? (answer in alphabetical order)": "HEAT AND PRESSURE",
+    "Giving your answer as north, south, east, or west, a southerly wind blows FROM what direction?": "SOUTH",
+    "Which of the following clouds has little vertical development: altostratus, altocumulus, cirrus, or nimbostratus": " CIRRUS",
+    "A weather forecast of 100% relative humidity suggests an increased possibility of: rain, wind, rising tempurature, or drying conditions": "RAIN",
+    "What is the name for all Great Circles that pass through the north and south poles?": "",
+    "What northward-moving ocean current is the dominant feature generally located off the east coast of the U.S.?": "GULF STREAM",
+    "What layer of Earth’s atmosphere starts at about the stratopause and extends upwards to about 55 miles above the Earth?": "MESOSPHERE",
+    "What numerical albedo value is given to a surface that reflects 100% of incident light?": "1",
+    "Marble is a metamorphic rock primarily composed of:": "CALCITE",
+		"What unstable substance does the stratosphere contain that functions to protect life on Earth?": "Ozone"
+}
+MathQ1 = {
+    "Name all of the following 5 numbers that are NOT prime numbers: 2; 5; 17; 33; 37": "33",
+    "What is the length, in inches, of one side of a square if the diagonal measures 7*sqrt(2) ?": "7",
+    "If the area of a rectangle is 200 centimeters squared and the length is 20 centimeters, what is the perimeter of the rectangle, in centimeters?": "60",
+    "If a line has a slope of -0.1, what is the slope of every line that is perpendicular to it?": "10",
+    "Consider an isosceles triangle whose vertex angle measures 20º. Find the measure of one of the EXTERIOR angles, in degrees, at the base of the triangle:": "100",
+    "If a car travels at a rate of 50 miles per hour, what is the distance, in miles, traveled in 5 hours?": "250",
+    "Subtract the following 2 polynomials and simplify:(6x^2 – 7x + 13) – (–x^2 + 4x – 3)": " 7x^2 – 11x + 16",
+    " If an ordered pair has an x-coordinate of zero, its graph lies on what part of the coordinate plane?": "Y-AXIS",
+    "If 2 coplanar parallel lines are intersected by a transversal line, what is the sum of all angles formed?": "720",
+    "Malcolm drove his car a distance of 2 miles in a time of 2 minutes 30 seconds. Find his average speed, in miles per hour:": "48",
+    "Solve the following equation for x: 15x + 8x – 3x = 120": "6",
+    "Evaluate the following expression: 10 + [8 + (2 – 10)] ": "10",
+    "Evaluate x/y when x = 0 and y = –8:": "0"
+}
+PhysicalSQ1 = {
+    "You are given a triangularly shaped block of copper and asked the volume of the block. Unable to remember any geometric volume formulas, you use a balance and find the block has a mass of 6.742 kilograms. Given that the density of copper is 8.96 grams per milliliter, what is the volume in milliliters of the block you were given, to the nearest 10 milliliters?": "750",
+    "If 40 grams of water at 0° Celsius is added to 10 grams of water at 100° Celsius, what will be the final temperature of the mixture, in degrees Celsius?": " 20°",
+    "How many valence electrons do the alkaline earth metals possess?": "2",
+    "Four identical 4 ohm resistors are connected in series. What is the total resistance in ohms?": "16",
+    "What is the SI unit of inductance?": "HENRY",
+    "If a car goes from 0 to 20 meters per second in 8 seconds, providing your answer as a decimal to the nearest tenth, what is its average acceleration in meters per second squared?": "2.5",
+    "What is the ideal mechanical advantage of a ramp that is 24 meters long and 4 meters higher at one end versus the other?": "6",
+    "In color synthesis using visible light, what is the most common term for the colors produced by combining equal proportions of two primary colors?": "SECONDARY",
+    "What modern solid-state electronic device is used as an amplifier in modern radios and, through its wide use as an electronic switch, is a basic component in modern logic circuits?": "TRANSISTOR",
+    "How many carbons are there in an ethanol molecule?": "2",
+    "What is the density, in grams per cubic centimeter, of a sample of plastic that floats with 45% of its volume submerged in pure water?": "0.45",
+    "Ignoring friction, what is the acceleration, in meters per second squared, when 1,000 newtons of force are applied to move a 200-kilogram crate across a level floor?": "5",
+    "To the first decimal place, what is the ideal mechanical advantage of a lever with a resistance arm of 200 centimeters and an effort arm of 3.4 meters?": "1.7",
+    "What is the percent efficiency, rounded to the nearest whole number, of a simple machine that requires 600 joules of energy input to achieve 400 joules of energy output?": "67%"
+}
+GeneralSQ1 = {
+    "What is the average internal Celsius temperature of the human body?": "37",
+    "What is the SI base unit used for the intensity of a light source?": "CANDELA",
+    "What is the astronomical term for the orbital arrangement of a superior planet with reference to the positions of the Sun and Earth, at which time the superior planet will usually appear its brightest?": "OPPOSITION",
+    "What is the name of the atomic process that provides the energy for a star to shine?": "FUSION",
+    "The zodiac is typically considered to be made of how many constellations?": "12",
+    "Convert 5 meters per second to kilometers per hour": "18",
+    "What is the common name for the organic solvent which is the main ingredient in nail polish remover?": "ACETONE",
+    "What element makes up about 24% of the Sun’s mass?": "HELIUM",
+    "Rounded to the nearest whole number, how many metersper second is equal to 40 kilometers per hour?": "11",
+    "What is the common term for the most accepted theory on how the universe came into being?": "BIG BANG",
+    "What letter is most often used by scientists as a symbol for light or the speed of light?": "C",
+    " Rounded to the nearest whole number, what is the specifi gravity of water at standard temperature and pressure?": "1",
+    "Rounded to the nearest whole number, what is absolute zero in degrees Celsius?": "-273",
+    "Convert 1 million centimeters per hour into kilometers per hour": "10",
+    "What is the astronomical term for the bright trail of a meteoroid passing through the atmosphere of the Earth?": "METEOR"
+}
+
